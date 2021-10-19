2e70670cb99dbffd47a9bce03f037692a60e44e7
Cristian <muser.talk@gmail.com>
2021-10-19 14:08:33 +0300

    Added hello handlers and serve it as a route.

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/dev-muser/learning_go
+
+go 1.17
--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/dev-muser/learning_go/handlers"
+)
 
 func main() {
-	fmt.Println("it's ok, works.")
+
+	l := log.New(os.Stdout, "product-api ", log.LstdFlags)
+	hello_handler := handlers.NewHello(l)
+
+	servemux := http.NewServeMux() // HTTP request multiplexer
+	servemux.Handle("/hello", hello_handler)
+	fmt.Println("Server is up and running.")
+	http.ListenAndServe(":7777", servemux)
+
 }

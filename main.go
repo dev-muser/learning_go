61b0aebdca54b3e34061c37b7eac0ae5e3341933
Cristian <muser.talk@gmail.com>
2021-10-19 14:36:20 +0300

    Added goodbye handler and registered as new route.

--- main.go
+++ main.go
@@ -13,9 +13,12 @@ func main() {
 
 	l := log.New(os.Stdout, "product-api ", log.LstdFlags)
 	hello_handler := handlers.NewHello(l)
+	goodbye_handler := handlers.NewGoodbye(l)
 
 	servemux := http.NewServeMux() // HTTP request multiplexer
 	servemux.Handle("/hello", hello_handler)
+	servemux.Handle("/goodbye", goodbye_handler)
+
 	fmt.Println("Server is up and running.")
 	http.ListenAndServe(":7777", servemux)
 
